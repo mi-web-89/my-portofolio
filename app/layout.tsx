1cd88f309e1f5dd9e31244376c6aa57bc2587d40
ismail <mi272401@gmail.com>
2026-01-22 15:21:47 +0700

    refactor : setup base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Header from "./component/Header"
+import Header from "./component/Header";
 import Footer from "./component/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
-  title: "Portofolio keren",
-  description: "Dibuat dengan Next.js oleh seorang developer hebat!",
+  title: "Muhammad Ismail",
+  description: "My Portofolio - Tech enthusiast and Frontend developer",
 };
 
 export default function RootLayout({
@@ -19,14 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter} bg-slate-900 text-slate-100`}>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow container mx-auto p-4">
-            {children}
-          </main>
-          <Footer />
-        </div>
+      <body className={`${inter} font-sans`}>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
