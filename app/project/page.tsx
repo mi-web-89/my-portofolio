98ba260c6e6f3cccb4df244755002ee4526f08b6
ismail <mi272401@gmail.com>
2026-01-28 16:34:43 +0700

    refactor: remove contact and project page

--- app/project/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectPage() {
-  return (
-    <div className="mx-auto mt-10 mb-6 px-6 py-4 w-1/2 text-center text-lg rounded-xl bg-slate-300">
-      <h1>Halaman Project</h1>
-    </div>
-  )
-}
